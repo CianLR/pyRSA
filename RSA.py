c5a49be1ff8a632931410d0cf7db03c84644cf3a
Cian Ruane <cian.ruane9@mail.dcu.ie>
2015-12-14 23:22:09 +0000

    Add basic RSA class structure

---
+++ RSA.py
@@ -0,0 +1,22 @@
+
+class RSA:
+    def __init__(self, bits):
+        return
+    
+    def extended_euclidean(self, totn, prime):
+        return
+
+    def gen_primes(self, bits):
+        return
+
+    def encrypt(self, message):
+        return
+
+    def decrype(self, cypher):
+        return
+
+    def get_public_key(self):
+        return
+
+    def get_private_key(self):
+        return
